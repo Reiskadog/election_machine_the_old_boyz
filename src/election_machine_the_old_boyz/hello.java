3ef399368e9978a464d826aa111c5704cd1df56c
JuusoMakisalo <81417538+JuusoMakisalo@users.noreply.github.com>
2021-03-31 10:55:22 +0300

    Java code
    Test

---
+++ src/election_machine_the_old_boyz/hello.java
@@ -0,0 +1,10 @@
+package election_machine_the_old_boyz;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
